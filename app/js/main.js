d9daba73bf7e45e62d3c4db5152fb47cbdc4a5d5
AmbalbIsh <AmbalbIsh@gmail.com>
2020-04-20 14:51:26 +0300

    added new-items page

--- app/js/main.js
+++ app/js/main.js
@@ -13,5 +13,25 @@ $(function () {
     slidesToScroll: 4
   });
 
+  $("#example_id").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 1000,
+    from: 0,
+    to: 600,
+    prefix: "$"
+  });
+
+  $('.icon-th-list').on('click', function(){
+    $('.products__item').addClass('list');
+    $('.icon-th-list').addClass('active');
+    $('.icon-th-large').removeClass('active');
+  });
+  $('.icon-th-large').on('click', function(){
+    $('.products__item').removeClass('list');
+    $('.icon-th-large').addClass('active');
+    $('.icon-th-list').removeClass('active');
+  });
+
   var mixer = mixitup('.products__inner-box');
 });
